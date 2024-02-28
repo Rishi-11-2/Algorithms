e89f9dd845bedad9d0d17ca17dbf0c77b5794f84
Rishi <rishichowdhury711@gmail.com>
2024-02-28 18:53:00 +0530

    OK

--- String/advancedHashing.cpp
+++ String/advancedHashing.cpp
@@ -44,7 +44,14 @@ struct PolyHash{
             if (suff[i] >= mod) suff[i] -= mod;
         }
     }
- 
+    /*  pref[r+1]=( p^r+1 + a[0]*p^(r) + a[1]*p^(r-1) + a[2]*p^(r-2) + ..+ a[l-1]*p^((r+1)-(l-1+1))+a[l]*p^((r+1)-(l+1))  .. a[r-1]*p + a[r] )
+        pref[l]=( p^l + a[0]*p^(l-1) + a[1]*p^(l-2) + a[2]*p^(l-3) + ...a[l-2]*p +  a[l-1])
+
+        p^(r-l+1)*pref[l] = ( a[0]*p^(r) + a[1]*p^(r-1) +.... + a[l-1]*p^(r-l+1) )
+
+        pref[r+1]-pref[l]=a[l]*p^(r-l)+ a[l+1]*p^(r-l-1)+.... + a[r-1]*p + a[r]
+
+    */
     PolyHash(const char* str)
         : PolyHash(vector<char> (str, str + strlen(str))) {}
  
