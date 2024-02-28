3aecde878f32846d50ed77f4cdbce38e7b35ca7f
Rishi <rishichowdhury711@gmail.com>
2024-02-28 18:55:18 +0530

    Hashing techniques along with explanation

--- String/advancedHashing.cpp
+++ String/advancedHashing.cpp
@@ -44,6 +44,7 @@ struct PolyHash{
             if (suff[i] >= mod) suff[i] -= mod;
         }
     }
+    /*https://usaco.guide/gold/string-hashing?lang=cpp*/
     /*  pref[r+1]=( p^r+1 + a[0]*p^(r) + a[1]*p^(r-1) + a[2]*p^(r-2) + ..+ a[l-1]*p^((r+1)-(l-1+1))+a[l]*p^((r+1)-(l+1))  .. a[r-1]*p + a[r] )
         pref[l]=( p^l + a[0]*p^(l-1) + a[1]*p^(l-2) + a[2]*p^(l-3) + ...a[l-2]*p +  a[l-1])
 
